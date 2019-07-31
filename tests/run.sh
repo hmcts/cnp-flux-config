24b2fbb2f9b955faa2f144774eb6bb0a4afae388
Tim Jacomb <tim.jacomb@hmcts.net>
2019-07-31 15:38:52 +0100

    Fix typo

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# see https://github.com/coryb/osht for docs
 eval "$(curl -q -s https://raw.githubusercontent.com/coryb/osht/master/osht.sh)"
 
+# expected number of tests, (in case any crash occurs)
 PLAN 1
 
 prod_check_present=$(grep -c "k8s/prod/ @hmcts/production-apps-approvals" CODEOWNERS)
