6a48a5d7d6044931d90449fd263dbc9f6bafeb57
Tim Jacomb <tim.jacomb@hmcts.net>
2019-07-31 15:27:44 +0100

    Add pipeline for testing changes

---
+++ tests/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(curl -q -s https://raw.githubusercontent.com/coryb/osht/master/osht.sh)"
+
+PLAN 2
+
+prod_check_present=$(grep -c "k8s/prod/ @hmcts/production-apps-approvals" CODEOWNERS)
+
+IS $prod_check_present -eq 1
+IS $prod_check_present -eq 2
+
+
+
