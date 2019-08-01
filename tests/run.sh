48ac424e82cb166a353439991d8222f6810c4820
Tim Jacomb <tim.jacomb@hmcts.net>
2019-08-01 10:40:09 +0100

    Add pedantic test to block additional owners

--- tests/run.sh
+++ tests/run.sh
@@ -4,8 +4,10 @@
 eval "$(curl -q -s https://raw.githubusercontent.com/coryb/osht/master/osht.sh)"
 
 # expected number of tests, (in case any crash occurs)
-PLAN 1
+PLAN 2
 
 prod_check_present=$(grep -c "k8s/prod/ @hmcts/production-apps-approvals" CODEOWNERS)
+other_prod_check_not_present=$(grep -c "k8s/prod/ .*" CODEOWNERS)
 
 IS $prod_check_present -eq 1
+IS $other_prod_check_not_present -eq 1
