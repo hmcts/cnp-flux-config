4bb5c1bb3c1c9e08c76c5b7361607191623276f0
anand <gkdasari@gmail.com>
2019-11-29 10:59:38 +0000

    Added sealed secret script for Demo ccd

--- k8s/demo/common/ccd/bin/vault-to-sealedsecret.sh
+++ k8s/demo/common/ccd/bin/vault-to-sealedsecret.sh
@@ -8,29 +8,6 @@
 # eg, run as follows on Demo
 # vault-to-sealedsecret.sh aat /workspace/hmcts/cnp-flux-config/k8s/demo/pub-cert.pem ccd
 
-env=$1
-cert=$2
-namespace=$3
-secrets=(
-    'ccd|ccd-api-gateway-oauth2-client-secret'
-    'ccd|ccd-admin-web-oauth2-client-secret'
-)
 
-for i in "${secrets[@]}"; do
-    vault=$(echo "$i" | awk -F'|' '{print $1}')-$env
-    name=$(echo "$i" | awk -F'|' '{print $2}')
-    secret=$(az keyvault secret show --vault-name $vault --name $name -o tsv --query value)
-    #echo ${secret}
-
-    # kubectl delete secret $vault-$name
-
-    kubectl create secret generic $name \
-        --from-literal key=$secret \
-        --namespace $namespace \
-        --dry-run -o json > $name.json
-
-    kubeseal --format=yaml --cert=$cert < $name.json > ../sealed-secrets/$name.yaml
-
-    # rm -f $team-$name.json
 
 done
