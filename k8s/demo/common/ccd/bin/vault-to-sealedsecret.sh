d417b02e55cdc5d36256d027a5593e61aff1b2df
anand <gkdasari@gmail.com>
2019-11-29 11:00:51 +0000

    Added sealed secret script for Demo ccd

--- k8s/demo/common/ccd/bin/vault-to-sealedsecret.sh
+++ k8s/demo/common/ccd/bin/vault-to-sealedsecret.sh
@@ -1,13 +1,2 @@
 #!/bin/bash -x
-# Note:  This script is just place holder for now. Do not use
-
-
-# script to pick up the secret from azure vault from the key provided 
-# <eg., ccd-api-gateway-oauth2-client-secret>  and create encrypted secret
-# which will be used in Kubernetes cluster. 
-# eg, run as follows on Demo
-# vault-to-sealedsecret.sh aat /workspace/hmcts/cnp-flux-config/k8s/demo/pub-cert.pem ccd
-
-
-
-done
+# Note:  This  is just place holder for now. WIP
