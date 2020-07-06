ed6bf3476ec8bc4a7de739e5afc9cd642ce48f75
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-07-06 15:37:33 +0100

    Fix var expansion + use array for errors

--- tests/flux-automation.sh
+++ tests/flux-automation.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -ex
+set -x
 
 _github_head_ref=$1
-_github_base_ref={$2:-master}
+_github_base_ref=${2:-master}
 
 whitelist_dirs=(
     admin
@@ -13,7 +13,7 @@ whitelist_dirs=(
 
 [ -z "$_github_head_ref" ] && echo "Error: github commit sha missing." && exit 1
 
-_errors=""
+_errors=()
 
 for f in $(git diff-tree --no-commit-id --name-only -r "$_github_head_ref" "$_github_base_ref")
 do
@@ -29,12 +29,12 @@ do
   
   # check if automated
   fgrep -E -q '(flux\.weave\.works|fluxcd\.io)/automated: *"true"' "$f"
-  [ $? -ne 0 ] && _errors="${_errors}${f}: automated must be set to true\n"
+  [ $? -ne 0 ] && _errors+="${f}: automated must be set to true"
   # check if prod tag
   fgrep -E -q '(filter\.fluxcd\.io|flux\.weave\.works)/(tag\.)*(java|nodejs): glob:prod-\*' "$f"
-  [ $? -ne 0 ] && _errors="${_errors}${f}: must use a prod-* tag\n"
+  [ $? -ne 0 ] && _errors+="${f}: must use a prod-* tag"
 done  
 
-[ ! -z "$_errors" ] && echo "$_errors" > /dev/stderr && exit 2
+[ -n "$_errors" ] && printf '%s\n' "${_errors[@]}" > /dev/stderr && exit 2
 
 exit 0
