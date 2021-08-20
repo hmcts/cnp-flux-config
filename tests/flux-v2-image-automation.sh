550654ee286d2574c052ec2c171c38574603893e
Thomas Thornton <>
2021-08-20 14:44:24 +0100

    Update IDAM Location

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -12,12 +12,12 @@ EXCLUSIONS_LIST=(
   apps/docmosis/docmosis/perftest.yaml
   apps/docmosis/docmosis/sbox.yaml
   apps/flux-system/base/image-automation-components.yaml
-  apps/idam-api/preview.yaml
-  apps/idam-api/sbox.yaml
-  apps/idam-web-admin/preview.yaml
-  apps/idam-web-admin/sbox.yaml
-  apps/idam-web-public/preview.yaml
-  apps/idam-web-public/sbox.yaml
+  apps/idam/idam-api/preview.yaml
+  apps/idam/idam-api/sbox.yaml
+  apps/idam/idam-web-admin/preview.yaml
+  apps/idam/idam-web-admin/sbox.yaml
+  apps/idam/idam-web-public/preview.yaml
+  apps/idam/idam-web-public/sbox.yaml
 )
 
 EXCLUSIONS=$(IFS="|" ; echo "${EXCLUSIONS_LIST[*]}")
