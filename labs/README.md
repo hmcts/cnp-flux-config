e856fa95fa40cb6e6cddd06f9a861a4661f72fb7
msl8r <72858053+msl8r@users.noreply.github.com>
2021-12-10 13:19:39 +0000

    Improve create script to allow for nodejs apps

---
+++ labs/README.md
@@ -0,0 +1,36 @@
+# Creating Flux config for your Golden Path Java app
+[Golden Path tutorial](https://backstage.platform.hmcts.net/catalog/default/component/golden-path-java) - VPN needed.
+## Creating the flux config for your Java lab application 
+
+Inside the labs/java directory run the `create-lab-flux-config.sh` script to create the flux config needed for your Java lab application.
+
+```shell
+cd labs/java/
+./create-lab-flux-config.sh 
+```
+
+The above script will do the following:
+- Create the directory that the new config will be created in `apps/labs/labs-'yourGitHubUsername'`
+- Create the imagePolicy and imageRepository config
+- Create the HelmRelease resource to deploy your app 
+- Add the image-policy and image-repo file references to the labs automation kustomization file so flux can automate image updates
+- Add a reference to the HelmRelease file the sbox kustomization cluster so the resources are 
+
+Once the script has been run, you will need to add and commit the new config and push it to GitHub. 
+- Run `git status` to check which files have been create and modified.
+- Use `git add` to stage the newly created files for commit
+- Run `git diff` to view the changes made to the existing kustomization files. You should see references to the newly created files for your lab app.
+- Once you're happy with the changes made, commit and push them and create a PR.
+
+More information on flux v2, its components and custom resources can be found on [flux docs site](https://fluxcd.io/docs/concepts/).
+
+## Deleting the flux config for your Java lab application
+
+Once you're finished with your lab app you can clean up the flux configuration but running the `clean-up-lab-flux-config.sh` script. 
+
+Inside the labs/java directory run the clean up script to remove the flux config you created earlier.
+
+```shell
+cd labs/java/
+./clean-up-lab-flux-config.sh 
+```
---
+++ labs/create-lab-flux-config.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+set -ex
+
+NAMESPACE=labs
+PRODUCT=labs
+COMPONENT=$1
+ACR=hmctssandbox
+NAMESPACE_DIR="../apps/${NAMESPACE}"
+COMPONENT_DIR="${NAMESPACE_DIR}/${PRODUCT}-${COMPONENT}"
+ENVIRONMENT="sbox"
+CWD=$PWD
+APPLICATION=${2}
+
+function usage() {
+  echo "Missing an input"
+  echo 'Usage: ./create-lab-flux-config.sh <component> <application>'
+  echo 'Component name - this should be your GitHub username'
+  echo "Application - java or nodejs"
+}
+
+if [ -z "${COMPONENT}" ] || [ -z "${APPLICATION}" ]; then
+  usage
+  exit 1
+fi
+
+# Create component dir and files
+if [ -d "${COMPONENT_DIR}" ]; then
+  echo "Lab directory already exists, run the cleanup script first"
+  exit 1
+else
+  cd ../
+  ./bin/v2/add-image-policies.sh ${NAMESPACE} ${PRODUCT} ${COMPONENT} ${ACR}
+  cd ${CWD}
+fi
+
+if [[ ${APPLICATION} == java ]]; then
+  INGRESS_HOST="${PRODUCT}-${COMPONENT}-{{ .Values.global.environment }}.service.core-compute-{{ .Values.global.environment }}.internal"
+elif [[ ${APPLICATION} == nodejs ]]; then
+  INGRESS_HOST="${PRODUCT}-${COMPONENT}-{{ .Values.global.environment }}.platform.hmcts.net"
+else
+  echo "Application type not recognised please use java or nodejs"
+fi
+
+# Create HR for lab
+(
+cat <<EOF
+---
+apiVersion: helm.toolkit.fluxcd.io/v2beta1
+kind: HelmRelease
+metadata:
+  name: labs-${COMPONENT}
+  namespace: labs
+spec:
+  releaseName: labs-${COMPONENT}
+  chart:
+    spec:
+      chart: ./stable/labs-${COMPONENT}
+      sourceRef:
+        kind: GitRepository
+        name: hmcts-charts
+        namespace: flux-system
+      interval: 1m
+  values:
+    ${APPLICATION}:
+      image: hmctssandbox.azurecr.io/labs/${COMPONENT}:latest #{"\$imagepolicy": "flux-system:labs-${COMPONENT}"}
+      ingressHost: ${INGRESS_HOST}
+      disableTraefikTls: true
+    global:
+      environment: sandbox
+      tenantId: "531ff96d-0ae9-462a-8d2d-bec7c0b42082"
+EOF
+) > "${COMPONENT_DIR}/${PRODUCT}-${COMPONENT}.yaml"
+
+if [[ ${APPLICATION} == "nodejs" ]]; then
+  export BACKEND="http://${PRODUCT}-${COMPONENT}-sandbox.service.core-compute-sandbox.internal"
+  yq eval  -i '(.spec.values.nodejs.environment.RECIPE_BACKEND_URL) = env(BACKEND) ' ${COMPONENT_DIR}/${PRODUCT}-${COMPONENT}.yaml 
+fi
+
+# Configure sbox to manage HR resource
+export NAMESPACE_PATH="../../${PRODUCT}-${COMPONENT}/${PRODUCT}-${COMPONENT}.yaml"
+
+if [[ $(yq eval '.spec[] | ( . == env(NAMESPACE_PATH))' ${NAMESPACE_DIR}/${ENVIRONMENT}/base/kustomization.yaml) =~ "true" ]]; then
+  echo "Reference to ../../${PRODUCT}-${COMPONENT}/${PRODUCT}-${COMPONENT}.yaml already exists ignoring.."
+else
+  yq eval -i '.resources += [env(NAMESPACE_PATH)]' ${NAMESPACE_DIR}/${ENVIRONMENT}/base/kustomization.yaml
+fi
