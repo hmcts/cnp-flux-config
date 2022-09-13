456ac71de727edc312f6dd92cd45a913c5583814
Damon Green <damon.green@btinternet.com>
2022-09-13 14:07:29 +0100

    exclude base file from tests

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -23,6 +23,7 @@ EXCLUSIONS_LIST=(
   apps/idam/idam-testing-support-api/sbox.yaml
   apps/probate/probate-cron-make-dormant-cases/demo.yaml
   apps/probate/probate-cron-reactivate-dormant-cases/demo.yaml
+  apps/probate/probate-cron-make-dormant-cases/probate-cron-make-dormant-cases.yaml
   k8s/namespaces/docmosis/docmosis/aat.yaml
   *demo.yaml
   k8s/demo/*
