147f95e40360051e8d848684391ccc7001c786c3
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2022-10-28 15:17:10 +0100

    DTSPB-3097 Reverted for some space commits

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -23,8 +23,10 @@ EXCLUSIONS_LIST=(
   apps/idam/idam-testing-support-api/sbox.yaml
   apps/probate/probate-cron-make-dormant-cases/demo.yaml
   apps/probate/probate-cron-reactivate-dormant-cases/demo.yaml
+  apps/probate/probate-cron-hmrc-extract/demo.yaml
   apps/probate/probate-cron-make-dormant-cases/probate-cron-make-dormant-cases.yaml
   apps/probate/probate-cron-reactivate-dormant-cases/probate-cron-reactivate-dormant-cases.yaml
+  apps/probate/probate-cron-hmrc-extract/probate-cron-hmrc-extract.yaml
   apps/probate/*
   k8s/namespaces/docmosis/docmosis/aat.yaml
   *demo.yaml
@@ -43,9 +45,9 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
     IMAGE_POLICIES=()
     for FILE in $(grep -lr "imagepolicy" $FILE_LOCATION | grep -Ev "$EXCLUSIONS" ); do
 
-        if [ $(yq eval '.kind' $FILE) != "HelmRelease" ]
+        if [ $(yq eval '.kind' $FILE) != "HelmRelease" ] 
         then
-            continue
+            continue 
         fi
 
         IFS=$'\n'
@@ -71,7 +73,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
             then
                 echo "No ImagePolicy for $IMAGE_POLICY in clusters/ptl-intsvc/base" && exit 1
             fi
-
+            
             IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
             IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern == "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
 
