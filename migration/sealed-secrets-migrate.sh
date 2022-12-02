34350db41d7990a1e20ade2fb2c490727c7549c3
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2022-12-02 14:22:56 +0000

    Move sealed secrets to flux v2 (#19580)

--- migration/sealed-secrets-migrate.sh
+++ migration/sealed-secrets-migrate.sh
@@ -2,65 +2,22 @@
 set -ex
 
 # Example of script ./migration/sealed-secrets-migrate.sh perftest camunda
-ENV=$1
-NAMESPACE=$2
+git clean -f apps/ clusters/ k8s/
+git checkout apps/ clusters/ k8s/
 
-for file in $(grep -lr "kind: SealedSecret" k8s/$ENV/common/$NAMESPACE  | grep -Ev "sealed-secrets"); do
+for file in $(grep -lr "kind: SealedSecret" k8s ); do
+  FILE_NAME=$(echo ${file//*\/})
+  ENV=$(echo $file | cut -d '/' -f2)
 
-    # Creates directory apps/<namespace>/<environment>
-    if [ ! -d "apps/$NAMESPACE/$ENV" ]
-    then
-        mkdir -p apps/$NAMESPACE/$ENV
+  NAMESPACE=$(yq eval '.metadata.namespace' $file)
+
+   if [[ $NAMESPACE == "default" ]]
+      then
+        NAMESPACE="neuvector"
     fi
 
-    # Creates directory apps/<namespace>/<environment>/base
-    if [ ! -d "apps/$NAMESPACE/$ENV/base" ]
-    then
-        mkdir -p apps/$NAMESPACE/$ENV/base
-    fi
-
-    # Creates file apps/<namespace>/<environment>/base/kustomization.yaml
-    if [ ! -f "apps/$NAMESPACE/$ENV/base/kustomization.yaml" ]
-    then
-(
-cat <<EOF
-apiVersion: kustomize.config.k8s.io/v1beta1
-kind: Kustomization
-resources:
-EOF
-) > "apps/$NAMESPACE/$ENV/base/kustomization.yaml"
-fi
-
-    # Creates directory apps/<namespace>/<environment>/sealed-secrets/
-    if [ ! -d "apps/$NAMESPACE/$ENV/sealed-secrets" ]
-    then
-        mkdir -p apps/$NAMESPACE/$ENV/sealed-secrets
-    fi
-
-    # If sealed-secret file is not there, then copy to apps/<namespace>/<environment>/sealed-secrets/
-    if [ ! -f "apps/$NAMESPACE/$ENV/sealed-secrets/$file" ]
-    then
-        mv $file apps/$NAMESPACE/$ENV/sealed-secrets/
-    fi
-
-    # Create kustomization file apps/<namespace>/<environment>/sealed-secrets/kustomization.yaml
-    if [ ! -f "apps/$NAMESPACE/$ENV/sealed-secrets/kustomization.yaml" ]
-    then
-(
-cat <<EOF
-apiVersion: kustomize.config.k8s.io/v1beta1
-kind: Kustomization
-resources:
-EOF
-) > "apps/$NAMESPACE/$ENV/sealed-secrets/kustomization.yaml"
-fi
-
-# Appends sealed-secrets name to apps/<namespace>/<environment>/sealed-secrets/kustomization.yaml
-FILE_NAME=($(echo "$file" | cut -d '/' -f5))
-NAMESPACE_PATH=$FILE_NAME yq eval -i '.resources += [env(NAMESPACE_PATH)]' apps/$NAMESPACE/$ENV/sealed-secrets/kustomization.yaml
-
-# Appends sealed-secrets name to apps/<namespace>/<environment>/base/kustomization.yaml
-NAMESPACE_PATH_ENVBASE="../sealed-secrets" yq eval -i '.resources += [env(NAMESPACE_PATH_ENVBASE)]' apps/$NAMESPACE/$ENV/base/kustomization.yaml
+  mv $file apps/$NAMESPACE/$ENV/base/
+  FILE_PATH="$FILE_NAME" yq eval -i '.resources += [env(FILE_PATH)]' apps/$NAMESPACE/$ENV/base/kustomization.yaml
 
 done
 
