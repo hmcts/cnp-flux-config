2e648e60883c4918a79d5d2ad6171c3285acbb03
Thomas Thornton <>
2022-12-02 11:20:26 +0000

    test

--- tests/flux-v2-kind-checks.sh
+++ tests/flux-v2-kind-checks.sh
@@ -3,15 +3,14 @@
 
 # for file in $(grep -lr "kind: HelmRelease*" apps/money-claims  --exclude-dir=base); do
 
-grep -lr "kind: HelmRelease*" apps/money-claims/cmc-legal-frontend
-
 for file in $(grep -lr "kind: HelmRelease*" apps/money-claims/cmc-legal-frontend); do
-  
+  KIND=()
   KIND=$(yq eval '.kind' $file)
+
     echo "KIND on file: $file is $KIND"
   if [ $KIND != "HelmRelease" ]
   then
-    echo "kind: should be HelmRelease and not $KIND in file $file." #&& exit 1
+    echo "kind: should be HelmRelease and not $KIND in file $file."
     yq e -i '.kind = "HelmRelease"' $file
   fi
 
