11363bf0d131ffecace4a57099232d4c2b3f668b
praveenadusumilli <adusumillipraveenkumar@gmail.com>
2022-12-19 17:04:39 +0000

    fix

--- tests/code-owner-check.sh
+++ tests/code-owner-check.sh
@@ -6,8 +6,8 @@ eval "$(curl -q -s https://raw.githubusercontent.com/coryb/osht/master/osht.sh)"
 # expected number of tests, (in case any crash occurs)
 PLAN 2
 
-kustomization_check_present=$(grep -c "apps/**/kustomization.yaml @hmcts/production-apps-approvals" CODEOWNERS)
-kustomize_check_present=$(grep -c "apps/**/kustomize.yaml @hmcts/platform-operations" CODEOWNERS)
+kustomization_check_present=$(grep -Fc "apps/**/kustomization.yaml @hmcts/platform-operations" CODEOWNERS)
+kustomize_check_present=$(grep -Fc "apps/**/kustomize.yaml @hmcts/platform-operations" CODEOWNERS)
 
-IS $prod_check_present -eq 1
+IS $kustomization_check_present -eq 1
 IS $kustomize_check_present -eq 1
