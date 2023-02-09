b3e21b11ebb89f98be10db97e4d5566a24f6fb49
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2023-02-09 14:41:58 +0000

    fixed issue

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -20,9 +20,8 @@ EXCLUSIONS_LIST=(
   apps/idam/idam-testing-support-api/sbox.yaml
   apps/probate/probate-cron-make-dormant-cases/probate-cron-make-dormant-cases.yaml
   apps/probate/probate-cron-reactivate-dormant-cases/probate-cron-reactivate-dormant-cases.yaml
-  apps/adoption/adoption-cron-draft-case-deletion-alert/demo/demo-image-policy.yaml
   apps/probate/*
-  apps/adoption/*/demo-.*
+  apps/adoption/*
   .*demo.*.yaml
   apps/*/*/demo
   apps/*/demo
