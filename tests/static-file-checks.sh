588b9f11220589d3f93797473bb1b7588eed3f41
Thomas Thornton <>
2023-04-26 15:27:36 +0100

    fix error [: too many arguments

--- tests/static-file-checks.sh
+++ tests/static-file-checks.sh
@@ -3,8 +3,10 @@
 # Script to check if apiVersion is present in all yaml files in apps/
 
 for FILE in $(find apps/* -type f -name "*.yaml"); do
-
-    if [ $(yq eval '.apiVersion' $FILE -N) == "test" ]
+    
+    API_VERSION_CHECK=$(yq eval -N '.apiVersion' $FILE -N)
+    
+    if [ "$API_VERSION_CHECK" == "null" ]
     then
         echo "apiVersion: missing in $FILE" && exit 1
     fi
