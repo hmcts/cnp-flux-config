e9c55a82fa2fb5be81866940e882967043c6d1e8
Thomas Thornton <>
2023-04-26 15:19:00 +0100

    whoops

--- tests/static-file-checks.sh
+++ tests/static-file-checks.sh
@@ -2,11 +2,11 @@
 
 # Script to check if apiVersion is present in all yaml files in apps/
 
-# for FILE in $(find apps/* -type f -name "*.yaml"); do
+for FILE in $(find apps/* -type f -name "*.yaml"); do
 
     if [ $(yq eval '.apiVersion' $FILE -N) == "test" ]
     then
         echo "apiVersion: missing in $FILE" && exit 1
     fi
 
-# done
+done
