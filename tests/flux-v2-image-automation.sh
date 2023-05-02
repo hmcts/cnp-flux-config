996cf7b302851ca3931bb2cb03e3c18f12342c48
Ben Neill <benjaminrneill@gmail.com>
2023-05-02 16:55:36 +0100

    whitelist

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -21,6 +21,7 @@ EXCLUSIONS_LIST=(
   apps/probate/probate-cron-make-dormant-cases/probate-cron-make-dormant-cases.yaml
   apps/probate/probate-cron-reactivate-dormant-cases/probate-cron-reactivate-dormant-cases.yaml
   apps/probate/*
+  apps/sscs/sscs-tribunals-frontend/*
   .*demo.*.yaml
   apps/*/*/demo
   apps/*/demo
