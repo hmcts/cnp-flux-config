abf9a69f3761627a1b3242abf75afb9bfa9f4e06
Rees Pozzi <reespozzi@icloud.com>
2023-09-08 10:58:43 +0100

    Correctly check it's null

--- bin/v2/add-namespace.sh
+++ bin/v2/add-namespace.sh
@@ -37,7 +37,7 @@ fi
 YAML_OBJECT=$(curl https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/team-config.yml | yq .${PRODUCT})
 
 # Check yaml object is returned
-if [ ! -n "$YAML_OBJECT" ]; then
+if [ "$YAML_OBJECT" == null ]; then
   echo "Error: YAML_OBJECT is empty. Unable to retrieve data from the URL."
   exit 1
 fi
