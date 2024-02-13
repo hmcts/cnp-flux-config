282d786052daf2e6fb569bca1ad44c7d0262d528
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-02-13 14:55:40 +0000

    Update script

--- tests/install-tools.sh
+++ tests/install-tools.sh
@@ -5,8 +5,8 @@ VERSION=v4.30.8
 BINARY=yq_linux_amd64
 
 # Download yq using the provided GitHub token
-wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - > yq.tar.gz
-
+wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - > \
+  tar xz && sudo mv ${BINARY} /usr/bin/yq
 # Check if the download was successful
 if [ $? -eq 0 ]; then
     echo "Authentication successful. Downloaded yq."
@@ -14,10 +14,6 @@ else
     echo "Error: Authentication failed or download unsuccessful."
     exit 1
 fi
-
-# Extract yq binary
-
-tar xz && sudo mv ${BINARY} /usr/bin/yq
 
 # Install kustomize
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh
