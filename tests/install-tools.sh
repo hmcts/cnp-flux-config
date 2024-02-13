f22ce24ff80af7dff4e4975b569afce9df43c2cb
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-02-13 15:04:32 +0000

    Debug

--- tests/install-tools.sh
+++ tests/install-tools.sh
@@ -5,8 +5,9 @@ VERSION=v4.30.8
 BINARY=yq_linux_amd64
 
 # Download yq using the provided GitHub token
-wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | \
-  tar xz && sudo mv ${BINARY} /usr/bin/yq
+wget_output=$(wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O -)
+echo "Downloaded file: $wget_output"
+
 # Check if the download was successful
 if [ $? -eq 0 ]; then
     echo "Authentication successful. Downloaded yq."
@@ -15,8 +16,16 @@ else
     exit 1
 fi
 
-# Install kustomize
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh
-chmod +x install_kustomize.sh
-rm -rf kustomize
-./install_kustomize.sh
+# Extract yq binary
+echo "$wget_output" | tar tz
+
+# Check if the downloaded file contains the expected binary
+if echo "$wget_output" | tar tz | grep -q "$BINARY"; then
+    echo "Found $BINARY in the downloaded tarball."
+else
+    echo "Error: $BINARY not found in the downloaded tarball."
+    exit 1
+fi
+
+# Move yq binary to /usr/bin
+echo "$wget_output" | tar xz && sudo mv ${BINARY} /usr/bin/yq
