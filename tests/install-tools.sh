f2b8676e5db5b43c9cf3bf84eadc30bbe31b4f0d
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-02-13 11:27:02 +0000

    Testing auth for install of yq

--- tests/install-tools.sh
+++ tests/install-tools.sh
@@ -3,8 +3,30 @@ set -e
 
 VERSION=v4.30.8
 BINARY=yq_linux_amd64
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | \
-  tar xz && sudo mv ${BINARY} /usr/bin/yq
+GITHUB_TOKEN=$GITHUB_TOKEN
 
-curl -s "https://raw.githubusercontent.com/\
-kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh && chmod +x install_kustomize.sh && rm -rf kustomize && ./install_kustomize.sh
+# get GitHub rate limit
+if [ -n "$GITHUB_TOKEN" ]; then
+    RATE_LIMIT=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/rate_limit)
+else
+    RATE_LIMIT=$(curl -s https://api.github.com/rate_limit)
+fi
+
+# get remaining rate limit
+REMAINING=$(echo "$RATE_LIMIT" | jq -r '.rate.remaining')
+
+# check if remaining requests are enough
+if [ "$REMAINING" -lt 2 ]; then
+    echo "Rate limit exceeded. Waiting for a minute..."
+    sleep 60
+fi
+
+# install yq
+wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | tar xz
+sudo mv ${BINARY} /usr/bin/yq
+
+# install kustomize
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh
+chmod +x install_kustomize.sh
+rm -rf kustomize
+./install_kustomize.sh
