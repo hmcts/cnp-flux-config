72188eedb45c9e1ad9ef62a744d9c574d35f6388
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-02-13 15:34:04 +0000

    simplify script again

--- tests/install-tools.sh
+++ tests/install-tools.sh
@@ -4,7 +4,8 @@ set -e
 VERSION=v4.30.8
 BINARY=yq_linux_amd64
 
-wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | tar xz && sudo mv ${BINARY} /usr/bin/yq
+wget -v --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | \
+  tar xz && sudo mv ${BINARY} /usr/bin/yq
 
 curl -s "https://raw.githubusercontent.com/\
 kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh && chmod +x install_kustomize.sh && rm -rf kustomize && ./install_kustomize.sh
