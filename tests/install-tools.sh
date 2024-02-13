6983ca58a4eaf05a9d10061a1b3b042863400228
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-02-13 15:54:33 +0000

    Add rate limiting checks back in

--- tests/install-tools.sh
+++ tests/install-tools.sh
@@ -1,11 +1,43 @@
+# #!/usr/bin/env bash
+# set -e
+
+# VERSION=v4.30.8
+# BINARY=yq_linux_amd64
+
+# wget -v --header="Authorization: token $AUTH_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | \
+#   tar xz && sudo mv ${BINARY} /usr/bin/yq
+
+# curl -s "https://raw.githubusercontent.com/\
+# kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh && chmod +x install_kustomize.sh && rm -rf kustomize && ./install_kustomize.sh
+
 #!/usr/bin/env bash
 set -e
 
 VERSION=v4.30.8
 BINARY=yq_linux_amd64
 
-wget -v --header="Authorization: token $AUTH_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | \
-  tar xz && sudo mv ${BINARY} /usr/bin/yq
+# Check GitHub rate limit
+if [ -n "$AUTH_TOKEN" ]; then
+    RATE_LIMIT=$(curl -s -H "Authorization: token $AUTH_TOKEN" https://api.github.com/rate_limit)
+else
+    RATE_LIMIT=$(curl -s https://api.github.com/rate_limit)
+fi
 
-curl -s "https://raw.githubusercontent.com/\
-kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh && chmod +x install_kustomize.sh && rm -rf kustomize && ./install_kustomize.sh
+# Extract remaining rate limit
+REMAINING=$(echo "$RATE_LIMIT" | jq -r '.rate.remaining')
+
+# Check if remaining requests are enough
+if [ "$REMAINING" -lt 2 ]; then
+    echo "Rate limit exceeded. Waiting for a minute..."
+    sleep 60
+fi
+
+# Download and install yq
+wget -q --header="Authorization: token $AUTH_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | tar xz
+sudo mv ${BINARY} /usr/bin/yq
+
+# Install kustomize
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh
+chmod +x install_kustomize.sh
+rm -rf kustomize
+./install_kustomize.sh
