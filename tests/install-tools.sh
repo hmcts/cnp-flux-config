fb5d786c3ce8e28ce63399daa69cac652767c1a4
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-02-13 15:32:42 +0000

    simplify script again

--- tests/install-tools.sh
+++ tests/install-tools.sh
@@ -4,22 +4,7 @@ set -e
 VERSION=v4.30.8
 BINARY=yq_linux_amd64
 
-# Download yq using the provided GitHub token
 wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | tar xz && sudo mv ${BINARY} /usr/bin/yq
 
-# Check if the download was successful
-if [ $? -eq 0 ]; then
-    echo "Authentication successful. Downloaded and installed yq."
-else
-    echo "Error: Authentication failed or download unsuccessful."
-    exit 1
-fi
-
-# Install kustomize
-echo "Installing kustomize..."
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh
-chmod +x install_kustomize.sh
-rm -rf kustomize
-./install_kustomize.sh
-
-echo "yq and kustomize installation completed successfully."
+curl -s "https://raw.githubusercontent.com/\
+kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize.sh && chmod +x install_kustomize.sh && rm -rf kustomize && ./install_kustomize.sh
