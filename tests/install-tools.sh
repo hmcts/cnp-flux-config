62f4d58452d1bcc886640dbaf18d9f91c5e4f480
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-02-13 15:09:42 +0000

    Updating to remove output

--- tests/install-tools.sh
+++ tests/install-tools.sh
@@ -5,33 +5,15 @@ VERSION=v4.30.8
 BINARY=yq_linux_amd64
 
 # Download yq using the provided GitHub token
-wget_output=$(wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O -)
-echo "Downloaded file: $wget_output"
+wget -q --header="Authorization: token $GITHUB_TOKEN" "https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz" -O - | tar xz && sudo mv ${BINARY} /usr/bin/yq
 
 # Check if the download was successful
 if [ $? -eq 0 ]; then
-    echo "Authentication successful. Downloaded yq."
+    echo "Authentication successful. Downloaded and installed yq."
 else
     echo "Error: Authentication failed or download unsuccessful."
     exit 1
 fi
-
-# Extract yq binary
-echo "Extracting yq binary..."
-echo "$wget_output" | tar tz
-echo "$wget_output" | tar xz
-
-# Check if the downloaded file contains the expected binary
-if echo "$wget_output" | tar tz | grep -q "$BINARY"; then
-    echo "Found $BINARY in the downloaded tarball."
-else
-    echo "Error: $BINARY not found in the downloaded tarball."
-    exit 1
-fi
-
-# Move yq binary to /usr/bin
-echo "Moving yq binary to /usr/bin..."
-echo "$wget_output" | tar xz && sudo mv ${BINARY} /usr/bin/yq
 
 # Install kustomize
 echo "Installing kustomize..."
