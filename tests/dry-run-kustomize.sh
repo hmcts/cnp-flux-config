51ef75683a8866625e909a1388d8376be8d53c41
Daniel Wilson <daniel.wilson@kainos.com>
2025-06-30 10:42:56 +0100

    debug

--- tests/dry-run-kustomize.sh
+++ tests/dry-run-kustomize.sh
@@ -68,10 +68,9 @@ if [[ -d "clusters/$ENVIRONMENT/$CLUSTER" ]]; then
     rm -rf CustomResourceDefinition-*
     cd "$CURRENT_DIRECTORY"
 
-    ls /tmp/schemas/ithc/00/master-standalone-strict/
+    #ls /tmp/schemas/ithc/00/master-standalone-strict/
     #cat /tmp/schemas/ithc/00/master-standalone-strict
-    curl -sL https://raw.githubusercontent.com/yannh/kubeconform/master/schema/kustomization-kustomize-v1.json -o ./kustomization-kustomize-v1.json
-    sed -i 's/"CLUSTER_NUMERIC": { "type": "string" }/"CLUSTER_NUMERIC": { "type": "integer" }/' ./kustomization-kustomize-v1.json
+    sed -i 's/"CLUSTER_NUMERIC": { "type": "string" }/"CLUSTER_NUMERIC": { "type": "integer" }/' tmp/schemas/ithc/00/master-standalone-strict/kustomization-kustomize-v1.json
 
     kubeconform "${kubeconform_config[@]}" "$TMP_DIR"
 fi
